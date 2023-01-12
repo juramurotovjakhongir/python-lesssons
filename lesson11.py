b2f45a3cb130b3806ddc7e3722c1df39238b6500
juramurotovjakhongir <122542989+juramurotovjakhongir@users.noreply.github.com>
2023-01-12 23:35:41 +0800

    Yangi fayllar qo'shildi
    11- va 9- darslar qo'shildi

---
+++ lesson11.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 10 21:36:41 2023
+
+@author: HP
+"""
+
+# 11-dars if- elif- else
+#son =-72
+#if son<0:
+#    print("Manfiy son") # -72 yozilganda manfiy son
+#else:
+#    print("Musbat son") 50 yozilganda musbat son
+
+# yosh = int(input('Yoshingiz nechida?'))
+# if yosh<=4:
+#   narh=0           #print('Sizga kirish bepul.')     
+# elif yosh<=12:
+#      narh=5000      #print('Sizha kirish 5000 so\'m')
+# elif yosh<=18:
+#       narh=8000    # print('Sizga kirish 8000 so\'m')
+# else:
+#       narh=10000           #print('Sizga kirish 10000 so\'m')
+# print(f"Sizga kirish {narh} so'm")
+   
+#kun= input("Bugun nima  kun?>>>")
+#if kun.lower()=='shanba' or kun.lower()=='yakshanba':
+#    print('Bugun dam olish kuni.')
+#else:
+#    print('Bugun ish kuni.')
+#Bugun nima kun?>>>shanba
+#Bugun dam olish kuni     
+
+#kun= input("Bugun nima  kun?")
+#harorat= float(input("Havo harorati qanday?"))
+
+#if kun.lower()=='yakshanba' and harorat>=30:
+#    print("Cho'milishga ketdik!")
+#elif kun.lower()=='yakshanba' and harorat<30:
+#    print("Uyda dam olamiz!")
+#Bugun nima  kun?yakshanba
+#Havo harorati qanday?40
+#Cho'milishga ketdik!
+
+#kun= input("Bugun nima  kun?")
+#harorat= float(input("Havo harorati qanday?"))
+
+#if (kun.lower()=='yakshanba' or kun.lower()=='shanba') and harorat>=30:
+#    print("Cho'milishga ketdik!")
+#elif (kun.lower()=='yakshanba' or kun.lower()=='shanba') and harorat<30:
+#    print("Uyda dam olamiz!")
+#Bugun nima  kun?shanba
+#Havo harorati qanday?36
+#Cho'milishga ketdik!
+
+#narh= 15000  #mujoz 15000 ming sumga taoom oldi
+#choy= True   # mijoz choy ham oldi
+#salat= False # mijoz salat olmadi
+
+#if choy and salat: # agar mijoz choy ham salat ham olgan bulsa
+#    narh = narh + 10000  # narhga 10000 sum qushiladi
+#elif choy or salat:      #agar mijoz choy yoki salat olgan bulsa
+#    narh = narh + 5000 # narhga 5000 qushamiz
+#    print(f"Jami {narh} so'm") #yakuniy narhni chiqaramizprint(narh)
+#Jami 20000 so'm   
+
+narh= 15000  #mujoz 15000 ming sumga taoom oldi
+choy= True   
+salat= False
+non=True
+kompot=True
+assorti= False
+#Quyidagi har bir shart alohida tekshiriladi va bir-biriga bog;liq emas!
+#if choy:
+#     narh = narh + 3000
+#    print("Mijoz choy oldi")
+#if salat: 
+#    narh = narh + 5000
+#if non:
+#    print("Mijoz non oldi")
+#    narh = narh + 2000
+#if kompot:
+#    print("Mijoz kompot oldi")
+#    narh = narh + 5000
+# if assorti:
+#     print("Mijoz assorti oldi")
+#     narh = narh + 15000
+# print(f"Jami {narh} so'm")
+# Mijoz choy oldi
+# Mijoz non oldi
+# Mijoz kompot oldi
+# Jami 25000 so'm    
+
+#menu = ['osh','qazonkabob','shashlik','norin','somsa']
+#'manti' in menu # menu da manti bormi?
+#'somsa' in menu
+#Out[3]: True 
+
+#menu = ['osh','qazonkabob','shashlik','norin','somsa']
+#ovqat= input('Nima ovqat yeysiz?>>>')
+#if ovqat.lower() in menu:
+#    print('Buyurtma qabul qilindi.')
+#else:
+#    print("Afsuski bizda bunday ovqat yo'q")
+#Nima ovqat yeysiz?>>>osh
+#Buyurtma qabul qilindi.  
+
+
+#menu = ['osh','qazonkabob','shashlik','norin','somsa']
+#buyurtmalar=["osh","somsa","manti","shashlik"]
+
+#if buyurtmalar:
+#   for taom in buyurtmalar:
+#       if taom in menu:
+#        print(f"Menuda {taom} bor")
+#   else:
+#        print(f"Kechirasiz, menuda {taom} yo'q")
+#else:
+#        print("Savatchangiz bo'sh")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
---
+++ lesson9/lesson9.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  8 22:45:32 2023
+
+@author: HP
+"""
+
+# 9-DARS FOR LOOP (2023.01.08)
+#mehmonlar=['Ali','Vali', 'Hasan' , 'Husan', 'Olim']
+#for mehmon in mehmonlar:
+    #print('Salom', mehmon)
+    #print('Hayr', mehmon)
+#Hayr Ali
+#Salom Vali
+#Hayr Vali
+#Salom Hasan
+#Hayr Hasan
+#Salom Husan
+#Hayr Husan
+#Salom Olim
+#Hayr Olim
+    
+#mehmonlar=['Ali','Vali', 'Hasan' , 'Husan', 'Olim']
+#for mehmon in mehmonlar:
+    #print(f'Hurmatli {mehmon}, sizni 20 Dekabr kuni nahorgi oshga chaqiramiz')
+    #print('Hurmat bilan, Zokirovlar oilasi\n')
+    #Hurmatli Ali, sizni 20 Dekabr kuni nahorgi oshga chaqiramiz
+#Hurmat bilan, Zokirovlar oilasi
+
+#Hurmatli Vali, sizni 20 Dekabr kuni nahorgi oshga chaqiramiz
+#Hurmat bilan, Zokirovlar oilasi
+
+#Hurmatli Hasan, sizni 20 Dekabr kuni nahorgi oshga chaqiramiz
+#Hurmat bilan, Zokirovlar oilasi
+
+#Hurmatli Husan, sizni 20 Dekabr kuni nahorgi oshga chaqiramiz
+#Hurmat bilan, Zokirovlar oilasi
+
+#Hurmatli Olim, sizni 20 Dekabr kuni nahorgi oshga chaqiramiz
+#Hurmat bilan, Zokirovlar oilasi
+    
+#sonlar=list(range(1,11)) #  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#for son in sonlar:
+#    print(f'{son} ning kvadrati {son**2} ga teng')
+#1 ning kvadrati 1 ga teng
+#2 ning kvadrati 4 ga teng
+#3 ning kvadrati 9 ga teng
+#4 ning kvadrati 16 ga teng
+#5 ning kvadrati 25 ga teng
+#6 ning kvadrati 36 ga teng
+#7 ning kvadrati 49 ga teng
+#8 ning kvadrati 64 ga teng
+#9 ning kvadrati 81 ga teng
+#10 ning kvadrati 100 ga teng
+    
+#sonlar=list(range(11))  
+#sonlar_kvadrati=[] #bo'sh ro;yxat yaratamiz
+#for son in sonlar:
+    #sonlar_kvadrati.append(son**2)
+    #print(sonlar)
+    #print(sonlar_kvadrati)
+    #[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+   # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+    
+#dostlar=[]
+#print("5 ta eng yaqin do'stingiz kim?")
+#for n in range(5): # n bu yerda 0 dN 4 GACHA QIYMATLARDI OLADI
+   #dostlar.append(input(f"{n+1}- do'stingizning ismini kiriting:"))
+#print(dostlar)
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
